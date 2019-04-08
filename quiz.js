0f208c874939d5d3183712adb8e07bbf32ba6d25
Brendan Meehan <bmmeehan3@gmail.com>
2019-04-07 21:50:47 -0400

    first commit didnt have actual changes- added actual changes

---
+++ quiz.js
@@ -0,0 +1,19 @@
+const STORE = [
+    {
+      question: 'Finish this phrase: \'No one expects the ___!\'',
+      choice1: 'FBI',
+      choice2: 'Spanish Inquisition',
+      choice3: 'Unexpected',
+      choice4: 'Dead Parrot',
+      correct: 'Spanish Inquisition'
+    },
+    {
+      question: 'Which member of the Monty Python troupe was responsible for the bizarre animation sequences?',
+      choice1: 'Terry Jones',
+      choice2: 'Erice Idle',
+      choice3: 'John Cleese',
+      choice4: 'Terry Gilliam',
+      correct: 'Terry Gilliam'
+    }
+  ];
+  
